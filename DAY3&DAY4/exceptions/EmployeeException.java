cebaf1afa38bc41847a826a2594ab7a978ce44f2
Mohammad Ahtisham <100846744+ahtishamkhan5678@users.noreply.github.com>
2022-10-02 12:09:29 +0530

    Day_3&4_Task_Completed ! ✅

---
+++ DAY3&DAY4/exceptions/EmployeeException.java
@@ -0,0 +1,13 @@
+package com.mgnrega.exceptions;
+
+public class EmployeeException extends Exception{
+	
+	public EmployeeException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public EmployeeException(String message) {
+		super(message);
+	}
+
+}
